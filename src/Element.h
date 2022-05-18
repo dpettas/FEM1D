bdacd1cf0bdc350d9ea157c8f5f15c0ebf30af8b
dpettas <dions.pettas@gmail.com>
2022-05-18 10:45:03 +0300

    Create the Brick Element type

---
+++ src/Element.h
@@ -0,0 +1,11 @@
+#ifndef _FEM_ELEMENT_INCLUDED_
+#define _FEM_ELEMENT_INCLUDED_
+
+class Element 
+{
+
+};
+
+
+#endif
+
---
+++ src/Elements/Brick.h
@@ -0,0 +1,23 @@
+#ifndef _FEM_BRICK_INCLUDED_
+#define _FEM_BRICK_INCLUDED_ 
+
+#include "Node3D.h"
+
+namespace FEM 
+{
+
+  class Brick 
+  {
+    public: 
+
+
+    private: 
+
+
+  };
+
+}
+
+
+
+#endif
