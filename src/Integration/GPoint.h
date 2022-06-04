d0aad03410f998525e196d567736bcf46e2a1b02
dpettas <dions.pettas@gmail.com>
2022-06-05 00:34:08 +0300

    Creating template GPoint.h

--- src/Integration/GPoint.h
+++ src/Integration/GPoint.h
@@ -2,5 +2,27 @@
 #define _FEM_GPOINT_INCLUDED_
 
 
+namespace FEM 
+{
 
+
+
+
+  template < typename ...Tv>
+  class GPoint 
+  {
+    public: 
+
+      GPoint();
+      GPoint(double weight, Tv...);
+
+      constexpr int size() const;
+
+    private: 
+
+      double  _weight;
+      double coors[sizeof...(Tv)];
+  };
+
+}
 #endif
