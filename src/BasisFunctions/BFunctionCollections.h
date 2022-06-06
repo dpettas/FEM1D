3468123f5b6045a1e2c101f15a2e7489bc6a9a8b
dpettas <dions.pettas@gmail.com>
2022-06-06 14:35:14 +0300

    Upded the BFunctionCollection

--- src/BasisFunctions/BFunctionCollections.h
+++ src/BasisFunctions/BFunctionCollections.h
@@ -6,10 +6,32 @@
 namespace FEM::COLLECTION 
 {
 
-  static BFunctionCollection LinearLagrange1DBFunction = 
+  static BFunctionCollection LinearLagrange1D = 
   {
     BFunction1D ( [] (double ksi) { return 0.50 * (1.0 - ksi);} ),
     BFunction1D ( [] (double ksi) { return 0.50 * (1.0 + ksi);} )
+  };
+
+
+  static BFunctionCollection LinearLagrange2D = 
+  {
+    BFunction2D ( [] (double ksi, double eta) { return 0.50 * (1.0 - ksi) * 0.50 * (1.0 - eta);} ),
+    BFunction2D ( [] (double ksi, double eta) { return 0.50 * (1.0 + ksi) * 0.50 * (1.0 - eta);} ),
+    BFunction2D ( [] (double ksi, double eta) { return 0.50 * (1.0 - ksi) * 0.50 * (1.0 + eta);} ),
+    BFunction2D ( [] (double ksi, double eta) { return 0.50 * (1.0 + ksi) * 0.50 * (1.0 + eta);} )
+  };
+  
+  static BFunctionCollection LinearLagrange3D = 
+  {
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 - ksi) * 0.50 * (1.0 - eta) * 0.50 * (1.0 - zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 + ksi) * 0.50 * (1.0 - eta) * 0.50 * (1.0 - zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 - ksi) * 0.50 * (1.0 + eta) * 0.50 * (1.0 - zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 + ksi) * 0.50 * (1.0 + eta) * 0.50 * (1.0 - zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 - ksi) * 0.50 * (1.0 - eta) * 0.50 * (1.0 + zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 + ksi) * 0.50 * (1.0 - eta) * 0.50 * (1.0 + zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 - ksi) * 0.50 * (1.0 + eta) * 0.50 * (1.0 + zeta);} ),
+    BFunction3D ( [] (double ksi, double eta, double zeta) { return 0.50 * (1.0 + ksi) * 0.50 * (1.0 + eta) * 0.50 * (1.0 + zeta);} ),
+
   };
   
 }
