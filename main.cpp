be6b439b980bc1e0f140d4643269a09b2df61b55
dpettas <dions.pettas@gmail.com>
2022-06-06 14:41:03 +0300

    Deleted Lagrange function classes

--- main.cpp
+++ main.cpp
@@ -6,9 +6,7 @@
 
 #include "Mesh3D.h"
 #include "BasisFunctions/BasisFunction.h"
-#include "BasisFunctions/LinearLagrange1D.h"
-#include "BasisFunctions/LinearLagrange2D.h"
-#include "BasisFunctions/LinearLagrange3D.h"
+#include "BasisFunctions/BFunctionCollections.h"
 #include "Integration/GPoint.h"
 #include "Integration/GPointCollection.h"
 #include "Integration/GPointCollections.h"
@@ -19,9 +17,7 @@
 
 int main(){
 
-
-  std::cout << FEM::COLLECTIONS::GPoints3_1D.get(2).get(1) << std::endl;
-  std::cout << FEM::COLLECTIONS::GPoints3_1D.get(2).weight() << std::endl;
+  FEM::COLLECTION::LinearLagrange1D.phi(0);
 
 
 
@@ -31,36 +27,6 @@ int main(){
 
 
 
-  FEM::LinearLagrange1DBFunction l1d;
-  FEM::LinearLagrange2DBFunction l2d;
-  FEM::LinearLagrange3DBFunction l3d;
-
-  auto phi = l3d.phi(0);
-
-  double x = +1;  
-  double y = -1;  
-  double z = +1;  
-
-  // point 0  (-1,-1,-1)
-  // point 1  (+1,-1,-1)
-  // point 2  (-1,+1,-1)
-  // point 3  (+1,+1,-1)
-  // point 4  (-1,-1,+1)
-  // point 5  (+1,-1,+1)
-  // point 6  (-1,+1,+1)
-  // point 7  (+1,+1,+1)
-
-  std::cout.precision(5);
-  std::cout << std::fixed;
-  
-  std::cout << "0" << "  " << l3d.phi(0)(x,y,z) << std::endl;
-  std::cout << "1" << "  " << l3d.phi(1)(x,y,z) << std::endl;
-  std::cout << "2" << "  " << l3d.phi(2)(x,y,z) << std::endl;
-  std::cout << "3" << "  " << l3d.phi(3)(x,y,z) << std::endl;
-  std::cout << "4" << "  " << l3d.phi(4)(x,y,z) << std::endl;
-  std::cout << "5" << "  " << l3d.phi(5)(x,y,z) << std::endl;
-  std::cout << "6" << "  " << l3d.phi(6)(x,y,z) << std::endl;
-  std::cout << "7" << "  " << l3d.phi(7)(x,y,z) << std::endl;
 
 
 
